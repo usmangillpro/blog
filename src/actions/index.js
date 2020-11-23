f1bfb0566498704be06d75926feec5d77c0bdf3f
usmangillpro <usmangillpro@gmail.com>
2020-11-23 15:20:19 +0500

    First Commit

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+import jsonplaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPosts = () => async (dispatch) => {
+  const response = await jsonplaceholder.get("/posts");
+
+  dispatch({
+    type: "FETCH_POSTS",
+    payload: response,
+  });
+};
